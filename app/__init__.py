de16aeaf71dc71fd32e21f9996ddc7f309ea1625
raulc03 <raul.casanova.03@gmail.com>
2022-04-02 01:38:17 -0500

    Project Created

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+import flask import Flask
+from flask_bootstrap import Bootstrap
+
+from .config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap = Bootstrap(app)
+    app.config.from_objetct(Config)
+    
+    return app
---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    # Creamos una llave secreta para desencriptar la cookie
+    SECRET_KEY = 'SUPER SECRETO'
