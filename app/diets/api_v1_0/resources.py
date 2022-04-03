35f817546f6b48422b31685712180ae70a4b86a3
raulc03 <raul.casanova.03@gmail.com>
2022-04-03 13:46:55 -0500

    Feat: Se agregó la clase dieta

--- app/diets/api_v1_0/resources.py
+++ app/diets/api_v1_0/resources.py
@@ -2,7 +2,7 @@ from flask import request, Blueprint
 from flask_restful import Api, Resource
 from app.common.error_handling import ObjectNotFound
 
-from app.diets.models import Child
+from app.diets.models import Child, Diet
 
 diets_v1_0_bp = Blueprint('diets_v1_0_bp', __name__)
 
@@ -13,13 +13,17 @@ class DietListResource(Resource):
     def get(self):
         args = request.args
 
-        # Aquí debe de realizar el filtro y llenar a la variable diet
-        diet = Child(int(args['age']), float(args['weight']),
-                     int(args['height']), int(args['activity']))
+        child = Child(int(args['age']), float(args['weight']),
+                      int(args['height']), int(args['activity']))
 
-        if diet is None:
+        diet = Diet(child)
+
+        # Función que obtiene la dieta filtrada
+        # dietList = diet.getDiets()
+
+        if dietList is None:
             raise ObjectNotFound('No existen dietas')
-        return diet.serialize()
+        return child.serialize()
 
 api.add_resource(DietListResource, '/api/v1.0/diets',
                  endpoint='diet_list_resource')
--- app/diets/models.py
+++ app/diets/models.py
@@ -1,9 +1,15 @@
+import pandas as pd
+
 class Child:
     def __init__(self, age, weight, height, activity):
         self.age = age
         self.weight = weight
         self.height = height
         self.activity = activity
+
+    # Función que calcula las macros del niño
+    def calculate_macro(self):
+        pass
 
     def serialize(self):
         return {
@@ -12,3 +18,12 @@ class Child:
                 'height': self.height,
                 'activity': self.activity
                 }
+
+
+class Diet:
+    def __init__(self, child):
+        self.child = child
+
+    # Agregar aquí las funciones para filtrar la dieta
+    def getDiets():
+        pass
