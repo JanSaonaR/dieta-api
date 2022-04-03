2363627c697ffde8e11e2d84afaef1ed24bf1231
raulc03 <raul.casanova.03@gmail.com>
2022-04-02 23:36:04 -0500

    Fix: Agregando el campo activity al modelo Child

--- app/diets/api_v1_0/resources.py
+++ app/diets/api_v1_0/resources.py
@@ -1,6 +1,8 @@
 from flask import request, Blueprint
 from flask_restful import Api, Resource
-from app.common.error_handling import ObjectNotFound 
+from app.common.error_handling import ObjectNotFound
+
+from app.diets.models import Child
 
 diets_v1_0_bp = Blueprint('diets_v1_0_bp', __name__)
 
@@ -10,15 +12,14 @@ api = Api(diets_v1_0_bp)
 class DietListResource(Resource):
     def get(self):
         args = request.args
-        print(args)
-        diet = {
-                "age": int(args['age']),
-                "weight": float(args['weight']),
-                "height": int(args['height'])
-                }
+
+        # Aquí debe de realizar el filtro y llenar a la variable diet
+        diet = Child(int(args['age']), float(args['weight']),
+                     int(args['height']), int(args['activity']))
+
         if diet is None:
             raise ObjectNotFound('No existen dietas')
-        return diet
+        return diet.serialize()
 
-api.add_resource(DietListResource, '/api/v1.0/diets',\
-        endpoint='diet_list_resource')
+api.add_resource(DietListResource, '/api/v1.0/diets',
+                 endpoint='diet_list_resource')
--- app/diets/models.py
+++ app/diets/models.py
@@ -1,5 +1,14 @@
 class Child:
-    def __init__(self, age, weight, height):
+    def __init__(self, age, weight, height, activity):
         self.age = age
         self.weight = weight
         self.height = height
+        self.activity = activity
+
+    def serialize(self):
+        return {
+                'age': self.age,
+                'weight': self.weight,
+                'height': self.height,
+                'activity': self.activity
+                }
